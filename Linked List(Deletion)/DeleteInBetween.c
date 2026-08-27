b7d0cc701cf2ea134d75a05d585621cc70f4419d
Pratik Kumar <pratikkumar1486@gmail.com>
2026-08-27 22:18:58 +0530

    Create DeleteInBetween.c

---
+++ Linked List(Deletion)/DeleteInBetween.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    struct node*next;
+    int data;
+};
+    void display(struct node*ptr){
+        while(ptr!=NULL){
+            printf("Elements are : %d\n",ptr->data);
+            ptr=ptr->next;
+            
+        }
+    }
+
+    struct node*delete(struct node*head,int index){
+        struct node*p=head;
+        int i=0;
+        while(i!=index-1){
+            p=p->next;
+            i++;
+        }
+        struct node*ptr=p->next;
+        p->next=ptr->next;
+        free(ptr);
+        return head;
+
+    }
+int main(){
+    struct node*head;
+    struct node*second;
+    struct node*third;
+    struct node*fourth;
+    head= (struct node*)malloc(sizeof(struct node));
+    second= (struct node*)malloc(sizeof(struct node));
+    third= (struct node*)malloc(sizeof(struct node));
+    fourth= (struct node*)malloc(sizeof(struct node));
+    head->data=21;
+    head->next=second;
+
+    second->data=1;
+    second->next=third;
+
+    third->data=2;
+    third->next=fourth;
+
+    fourth->data=210;
+    fourth->next=NULL;
+    head = delete(head,1);
+    display(head);
+
+
+    return 0;
+}
