5ce0ce6bf00f4f5233857de4e928af7f919485f5
Pratik Kumar <pratikkumar1486@gmail.com>
2026-09-07 01:37:55 +0530

    Create insertingAtlast.c

---
+++ Doble linked list/insertingAtlast.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node*prev;
+    struct node*next;
+};
+    void display(struct node*head){
+        struct node*ptr=head;
+        while(ptr!=NULL){
+            printf("%d ",ptr->data);
+            ptr=ptr->next;
+        }
+            printf("\n");
+    }
+    struct node*insert(struct node*head,int data){
+        struct node*ptr=(struct node*)malloc(sizeof(struct node));
+        struct node*p=head;
+        ptr->data=data;
+        ptr->next=NULL;
+        
+        while(p->next!=NULL){
+            p=p->next;
+        }
+        p->next=ptr;
+        ptr->prev=p;
+        return head;
+    }
+
+int main(){
+    struct node*head=(struct node*)malloc(sizeof(struct node));
+    struct node*second=(struct node*)malloc(sizeof(struct node));
+    struct node*third=(struct node*)malloc(sizeof(struct node));
+    //linking ->
+    head->prev=NULL;
+    head->data=21;
+    head->next=second;
+    second->prev=head;
+    second->data=12;
+    second->next=third;
+    third->prev=second;
+    third->data=34;
+    third->next=NULL;
+    printf("before insertion : \n");
+    display(head);
+    printf("after insertion : \n");
+    head = insert(head,65);
+    display(head);
+    return 0;
+}
